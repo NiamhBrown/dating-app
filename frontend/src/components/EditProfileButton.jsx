30c7cb7accb070d7b707c45713e6b428c8dbf9d4
emily <xiangnan.liu@yahoo.co.uk>
2024-06-13 15:39:24 +0100

    backend-services-updated

---
+++ frontend/src/components/EditProfileButton.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+const EditProfileButton = () => {
+  const navigate = useNavigate();
+
+  const handleProfileButton = () => {
+    navigate("/profile/edit");
+  };
+  return (
+    <>
+      <button
+        name="edit-profile-button"
+        id="edit-profile-button"
+        onClick={handleProfileButton}
+      >
+        Edit profile
+      </button>
+    </>
+  );
+};
+
+export default EditProfileButton;

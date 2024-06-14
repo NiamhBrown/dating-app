d86986a02188740400e7fe8f591b67f9d43cb3e2
Mohammed Ali Zaini <mazaini93@gmail.com>
2024-06-14 16:01:26 +0100

    login

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../../services/authentication";
 import './Login.css';
+import ConsoleText from "./ConsoleText";
 
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -24,15 +25,25 @@ export const LoginPage = () => {
   return (
     <>
       <div className="loginpage">
-        <h2 role="title" id="logintitle" className="logintitle">Login</h2>
+        {/* <h2 role="title" id="logintitle" className="logintitle">Login</h2> */}
+        <ConsoleText
+        words={['Console.login', 'touchUsername', 'touchPassword', 'pressEnter']}
+        colors={['dimgrey', 'dimgrey', 'dimgrey', 'dimgrey']}
+      />
         <form className="formcontainer" onSubmit={handleSubmit}>
+        <img src="../../../public/backgroundimages/Devlink.png" alt="Devlink Logo" className="devlinklogo" />
+          <br />
           <label htmlFor="email">Email:</label>
           <input id="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <br />
           <label htmlFor="password">Password:</label>
           <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <input role="submit-button" id="submit" type="submit" value="Submit" />
           <br />
-          <p>Don't have an account? Sign up <Link to="/">here.</Link></p>
+          <button className="btn draw-border" role="submit-button" id="submit" type="submit" value="Submit">Submit</button>
+          <br />
+          <br />
+          <br />
+          <p className="navlogin">Don't have an account? Sign up <Link to="/">here!</Link></p>
         </form>
       </div>
     </>

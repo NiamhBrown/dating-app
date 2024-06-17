7f6a2014920540a76c9edf7e9356460db79a44a3
Umut <umutc901@gmail.com>
2024-06-17 16:21:24 +0100

    end to end encryption added to backend and frontend

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,21 +1,30 @@
 const User = require("../models/user");
-const { generateToken } = require("../lib/token");const createToken = async (req, res) => {
+const { generateToken } = require("../lib/token");
+
+const createToken = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   try {
     const user = await User.findOne({ email: email });
     if (!user) {
       console.log("Auth Error: User not found");
-      res.status(401).json({ message: "User not found" });
+      return res.status(401).json({ message: "User not found" });
+
     }
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
       return res.status(401).send("Invalid email or password");
-    }    const token = generateToken(user.id);
+    }
+
+    const token = generateToken(user.id);
     res.status(201).json({ token: token, userId: user._id, message: "OK" });
   } catch (err) {
     res.status(500).send(err.message);
   }
-};const AuthenticationController = {
+};
+
+const AuthenticationController = {
   createToken: createToken,
-};module.exports = AuthenticationController;
+};
+
+module.exports = AuthenticationController;
--- api/controllers/chats.js
+++ api/controllers/chats.js
@@ -1,17 +1,25 @@
 const Chat = require("../models/chat");
+const crypto = require('crypto');
 
 const create = async (sender, recipient) => {
+  const encryptionKey = crypto.randomBytes(32).toString('hex');
   const chat = new Chat({
     usersInChat: [sender, recipient],
-    messagesArray: []
+    messagesArray: [],
+    encryptionKey
   });
   await chat.save();
+  console.log(encryptionKey);
 };
 
 const returnHistory = async (req, res) => {
-  console.log("chat hist")
+  console.log("chat hist");
   const currentChat = await Chat.findOne({ usersInChat: { $all: [req.body.sender, req.body.recipient] } });
-  res.status(200).json({ chatId: currentChat._id, history: currentChat.messagesArray });
+  if (!currentChat) {
+    res.status(404).json({ message: "Chat not found" });
+    return;
+  }
+  res.status(200).json({ chatId: currentChat._id, history: currentChat.messagesArray, encryptionKey: currentChat.encryptionKey });
 };
 
 const sendMessage = async (req, res) => {
